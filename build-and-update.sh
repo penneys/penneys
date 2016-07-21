3e9bc04f69b712e6a36d2b5ef7c2a1526e2e58dd
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-07-21 05:20:05 -0300

    Should have a deployment script

---
+++ build-and-update.sh
@@ -0,0 +1,15 @@
+# Builds locally and pushes to the published website.
+
+npm install
+gulp
+
+git clone --depth 1 git@github.com:penneys/penneys.github.io.git
+cp -r dist/* penneys.github.io/
+
+cd penneys.github.io
+git add .
+git commit -am "Updated the website"
+git push
+
+cd ..
+rm -rf contagium.github.io
